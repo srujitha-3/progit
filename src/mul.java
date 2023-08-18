4a5de41ab5e99e76c725b623d851b4bd61e28a3a
srujitha-3 <srujithanandivada@gmail.com>
2023-08-18 15:14:14 -0400

    mul

--- src/mul.java
+++ src/mul.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class mul {
+public class mul {
+    public static void main(String[] args){
+        int a=2;
+        int b=3;
+        int c=a*b;
+        System.out.println("the multiplication of numbers is:"+c);
+    }
 }
